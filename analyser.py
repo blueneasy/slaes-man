593e596223ebd272d791deb158957dbf7e4253b3
Michał <michal.piesiewicz@gmail.com>
2019-06-28 15:39:28 +0200

    catched exception when there is no ceneo recommendation

--- analyser.py
+++ analyser.py
@@ -2,6 +2,55 @@ import pandas as pd
 import xlrd
 import bs4
 import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+# selenium settings
+
+option = Options()
+option.add_argument("==disable-infobars")
+option.add_argument("==start-maximized")
+option.add_argument("--disable-extensions")
+option.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.notifications": 2})
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
+
+# functions:
+
+# 1) get product name and prices from c.pl.
+# note: Only one container displays for products without recommendation.
+
+
+def test1stlink():
+    links = cpage_soup.findAll('div', {'class:': 'cat-prod-row js_category-list-item js_clickHashData js_man-track-event'})
+
+    # fitem = cpage_soup.find('div', {'class': 'cat-prod-row-body'}).div.a.attrs['href']
+    i = 0
+    while i < 3:
+        if '/Click/Offer' in links[0].div.div.a.attrs['href']:
+            i += 1
+        else:
+            fitem = links.div.div.a.attrs['href']
+            print(fitem)
+            return fitem
+
+
+def scrapceneo():
+    boxes = mdpage_soup.findAll('section', {'class': 'product-offers-group'})
+    ceneoname = mdpage_soup.find('section', {'class': 'product-offers-group'}).table.tbody.tr.attrs[
+        'data-gaproductname']
+    print(ceneoname)
+    cena1 = mdpage_soup.find('section', {'class': 'product-offers-group'}).table.tbody.tr.attrs['data-price']
+    print(cena1)
+    if len(boxes) > 1:
+        cena2 = boxes[1].table.tbody.tr.attrs['data-price']
+        print(cena2)
+    else:
+        cena2 = 0
+
+    return ceneoname, cena1, cena2
 
 
 db = pd.read_excel('Products 2019-06-23.xlsx', index_col=0)
@@ -12,30 +61,26 @@ for nm in db['Produkt'].values:
 
     print(nm)
     myurl = 'https://www.ceneo.pl/;szukaj-' + nm.replace(' ', '+')
-    morele = requests.get(myurl)
-    mpage_html = morele.text
-    morele.close()
+    print(myurl)
+    ceneopage = requests.get(myurl)
+    cpage_html = ceneopage.text
+    ceneopage.close()
 
-    mpage_soup = bs4.BeautifulSoup(mpage_html, 'html.parser')
+    cpage_soup = bs4.BeautifulSoup(cpage_html, 'html.parser')
 
     # grab 1st product's link
-    fitem = mpage_soup.find('div', {'class': 'cat-prod-row-body'}).div.a.attrs['href']
-    print(fitem)
+
+    test1stlink()
 
     mydetailedurl = 'https://www.ceneo.pl' + fitem
-    moreledet = requests.get(mydetailedurl)
-    mdpage_html = moreledet.text
-    moreledet.close()
+    ceneodet = requests.get(mydetailedurl)
+    cdpage_html = ceneodet.text
+    ceneodet.close()
 
-    mdpage_soup = bs4.BeautifulSoup(mdpage_html, 'html.parser')
+    mdpage_soup = bs4.BeautifulSoup(cdpage_html, 'html.parser')
 
     # grab best prices
-    ceneoname = mdpage_soup.find('h1', {'class': 'product-name js_product-h1-link js_product-force-scroll js_searchInGoogleTooltip default-cursor'}).text
-    print(ceneoname)
-    cena1 = mdpage_soup.find('table', {'class': 'product-offers js_product-offers'}).tbody.tr.attrs['data-price']
-    print(cena1)
-    cena2 = mdpage_soup.find('table', {'class': 'product-offers js_product-offers js_normal-offers'}).tbody.tr.attrs['data-price']
-    print(cena2)
+    ceneoname, cena1, cena2 = scrapceneo()
 
     new_single_row = {}
     new_single_row.update({'ceneo': ceneoname, 'cena1': cena1, 'cena2': cena2})
